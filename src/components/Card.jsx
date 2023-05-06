49152088b2e1d5ba8757cef203f0558567c36fe4
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-05-06 22:25:42 +0200

    3D-Card-Framer-Motion-Tailwind

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Logo 
+import LogoImg from '../image/logo.svg';
+
+//Nike img
+import NikeImg from '../image/nike.png';
+
+const Card = () => {
+  return (
+    <div className='w-[426px] min-h-[600px] bg-[#e4dfdc]'>
+        Card
+    </div>
+  )
+}
+
+export default Card
