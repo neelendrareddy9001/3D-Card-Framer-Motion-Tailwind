4faaa4b5bb8f2be9a0e8f6c95ddb2eca3f4b9023
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-05-06 23:42:42 +0200

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-
-//Logo 
-import LogoImg from '../image/logo.svg';
-
-//Nike img
+//Nike image
 import NikeImg from '../image/nike.png';
 
-const Card = () => {
+//logo image
+import LogoImg from '../image/logo.svg';
+
   const Card = () => {
     const colors = [
         {value: "#b6a179"},
@@ -15,9 +13,43 @@ const Card = () => {
         {value: "#f2c758"},
         {value: "#ffffff"}
     ];
-    
   return (
-    
+    //card Wrapper
+        <div className='w-[426px] min-h-[600px] bg-[#e4dfdc] rounded-[30px] border-[4px] border-white px-[40px] py-[24px] cursor-pointer'>
+            {/* cardl logo */}
+            <div className='mb-6'>
+                <img src={LogoImg} alt=''/>
+            </div>
+
+            {/* cardl title */}
+            <div>title</div>
+
+            {/* cardl subtitle */}
+            <div>subtitle</div>
+
+            <div>
+                <button>button</button>
+                <div>price</div>
+            </div>
+
+            <ul>
+                {/*Colors*/}
+                {colors.map((color, index) => {
+                    return (
+                        <li
+                            key={index}
+                            className='w-8 h-8 rounded-full cursor-pointer'
+                            style={{backgroundColor: color.value}}
+                        ></li>
+                    )
+                } )}
+            </ul>
+
+            {/*Card Image*/}
+            <div>
+                <img src={NikeImg} alt='' draggable='false'/>
+            </div>
+        </div>
   )
 }
 
